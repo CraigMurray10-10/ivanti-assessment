ba6d2a8d4bcecc576b76280d685ff691a3ab4fbd
CraigMurray10-10 <craig.murray.2000@gmail.com>
2023-09-13 04:08:38 +0100

    Both ShapeService methods & passing all ShapeService tests

--- TechnicalTest.Core/Services/ShapeService.cs
+++ TechnicalTest.Core/Services/ShapeService.cs
@@ -7,15 +7,12 @@ namespace TechnicalTest.Core.Services
     {
         public Shape ProcessTriangle(Grid grid, GridValue gridValue)
         {
-            // TODO: Calculate the coordinates.
             string? row = gridValue.Row;
             int col = gridValue.Column;
 
-            // var V1 = Tuple.Create(0, 0);
-
-            Coordinate V1 = new(0, 0);
-            Coordinate V2 = new(0, 0);
-            Coordinate V3 = new(0, 0);
+            Coordinate V1 = new(0, 0); // V1: vertex at 90degree angle
+            Coordinate V2 = new(0, 0); // V2: vertex at leftmost 45degree angle
+            Coordinate V3 = new(0, 0); // V3: vertex at rightmost 45degree angle
 
             switch(row) {
                 case "A":
@@ -59,6 +56,66 @@ namespace TechnicalTest.Core.Services
                     V2.X = 0;
                     V3.X = 10;
                     break;
+                case 3:
+                    V1.X = 10;
+                    V1.Y = V3.Y;
+                    V2.X = 10;
+                    V3.X = 20;
+                    break;
+                case 4:
+                    V1.X = 20;
+                    V1.Y = V2.Y;
+                    V2.X = 10;
+                    V3.X = 20;
+                    break;
+                case 5:
+                    V1.X = 20;
+                    V1.Y = V3.Y;
+                    V2.X = 20;
+                    V3.X = 30;
+                    break;
+                case 6:
+                    V1.X = 30;
+                    V1.Y = V2.Y;
+                    V2.X = 20;
+                    V3.X = 30;
+                    break;
+                case 7:
+                    V1.X = 30;
+                    V1.Y = V3.Y;
+                    V2.X = 30;
+                    V3.X = 40;
+                    break;
+                case 8:
+                    V1.X = 40;
+                    V1.Y = V2.Y;
+                    V2.X = 30;
+                    V3.X = 40;
+                    break;
+                case 9:
+                    V1.X = 40;
+                    V1.Y = V3.Y;
+                    V2.X = 40;
+                    V3.X = 50;
+                    break;
+                case 10:
+                    V1.X = 50;
+                    V1.Y = V2.Y;
+                    V2.X = 40;
+                    V3.X = 50;
+                    break;
+                case 11:
+                    V1.X = 50;
+                    V1.Y = V3.Y;
+                    V2.X = 50;
+                    V3.X = 60;
+                    break;
+                case 12:
+                    V1.X = 60;
+                    V1.Y = V2.Y;
+                    V2.X = 50;
+                    V3.X = 60;
+                    break;
                 default:
                     break;
             }
@@ -74,7 +131,46 @@ namespace TechnicalTest.Core.Services
         public GridValue ProcessGridValueFromTriangularShape(Grid grid, Triangle triangle)
         {
             // TODO: Calculate the grid value.
-            return new GridValue(0, 0);
+            string? row;
+            int col;
+            string outerVertexPos;
+
+            Coordinate brv = triangle.BottomRightVertex;
+            Coordinate ov = triangle.OuterVertex;
+            switch(brv.Y) {
+                case 10:
+                    row = "A";
+                    break;
+                case 20:
+                    row = "B";
+                    break;
+                case 30:
+                    row = "C";
+                    break;
+                case 40:
+                    row = "D";
+                    break;
+                case 50:
+                    row = "E";
+                    break;
+                case 60:
+                    row = "F";
+                    break;
+                default:
+                    row = "";
+                    break;
+            }
+
+            if (brv.Y == ov.Y) {
+                outerVertexPos = "bottomLeft";
+                col = brv.X / 5 - 1;
+            }
+            else {
+                outerVertexPos = "topRight";
+                col = brv.X / 5;
+            }
+            
+            return new GridValue("" + row + col);
         }
     }
 }
