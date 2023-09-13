cb9dac5ed4cfd52c478efea3f70f552fe86d9daa
CraigMurray10-10 <craig.murray.2000@gmail.com>
2023-09-13 01:56:08 +0100

    First stage coordinates calc framework, passes tests 1-3

--- TechnicalTest.Core/Services/ShapeService.cs
+++ TechnicalTest.Core/Services/ShapeService.cs
@@ -8,11 +8,66 @@ namespace TechnicalTest.Core.Services
         public Shape ProcessTriangle(Grid grid, GridValue gridValue)
         {
             // TODO: Calculate the coordinates.
+            string? row = gridValue.Row;
+            int col = gridValue.Column;
+
+            // var V1 = Tuple.Create(0, 0);
+
+            Coordinate V1 = new(0, 0);
+            Coordinate V2 = new(0, 0);
+            Coordinate V3 = new(0, 0);
+
+            switch(row) {
+                case "A":
+                    V2.Y = 0;
+                    V3.Y = 10;
+                    break;
+                case "B":
+                    V2.Y = 10;
+                    V3.Y = 20;
+                    break;
+                case "C":
+                    V2.Y = 20;
+                    V3.Y = 30;
+                    break;
+                case "D":
+                    V2.Y = 30;
+                    V3.Y = 40;
+                    break;
+                case "E":
+                    V2.Y = 40;
+                    V3.Y = 50;
+                    break;
+                case "F":
+                    V2.Y = 50;
+                    V3.Y = 60;
+                    break;
+                default:
+                    break;
+            }
+
+            switch(col) {
+                case 1:
+                    V1.X = 0;
+                    V1.Y = V3.Y;
+                    V2.X = 0;
+                    V3.X = 10;
+                    break;
+                case 2:
+                    V1.X = 10;
+                    V1.Y = V2.Y;
+                    V2.X = 0;
+                    V3.X = 10;
+                    break;
+                default:
+                    break;
+            }
+
             return new Shape(new List<Coordinate>
             {
-                new(0, 0),
-                new(0, 0),
-                new(0, 0)
+                V1,
+                V2,
+                V3
             });
         }
 
